6c48f4a69f7e49fb05307f88d8f2fb38eac61b2e
JaeHun Jung <jjh44723@gmail.com>
2019-02-14 15:06:11 -0600

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -26,7 +26,7 @@ class MoviesController < ApplicationController
 
 
     Movie.find_each do |movie|
-      if @all_ratings.include?(movie.rating) == false
+      if @all_ratings.has_key?(movie.rating) == false
         @all_ratings.store(movie.rating, true)
       elsif params[:ratings] != nil
         @movies = Movie.where(rating: params[:ratings].keys)
