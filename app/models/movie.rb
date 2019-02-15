0a65db3b7a340db175c200569288bcf410338ccc
JaeHun Jung <jjh44723@gmail.com>
2019-02-14 23:32:17 -0600

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,15 @@
 class Movie < ActiveRecord::Base
 
-	def self.getmovie_rating arg1
+	def Movie.getmovie_rating arg1
 		@result = Movie.where(rating: arg1)
 	end
 
-	def self.getall_ratings
+	def Movie.getall_ratings
 		@movies = Movie.all
-		@all_ratings = Array.new
-		@movies.find_each do |movie|
-			if @all_ratings.include?(movie.rating) === false
-				@all_ratings.push(movie.rating)
+		@ratings = Array.new
+		@movies.each do |movie|
+			if @ratings.include?(movie.rating) === false
+				@ratings.push(movie.rating)
 			end
 		end
 	end
