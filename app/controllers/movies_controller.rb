40a1c27cb3eb0a28cf462fbc7642c269e138dd0f
JaeHun Jung <jjh44723@gmail.com>
2019-02-15 11:23:59 -0600

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,8 +14,12 @@ class MoviesController < ApplicationController
     
     @all_ratings = Movie.getall_ratings
 
-
-      session[:ratings] = params[:ratings].keys
+      if params[:ratings] != nil
+        session[:ratings] = params[:ratings].keys
+      else
+        session[:ratings] = params[:ratings]
+      end
+      
       @given_hash = session[:ratings]
       @given_hash = Movie.getall_ratings if session[:ratings] == nil
 
