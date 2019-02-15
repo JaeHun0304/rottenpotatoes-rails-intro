916ba026117d3d720d8a13be60536a498043bf78
JaeHun Jung <jjh44723@gmail.com>
2019-02-15 00:09:22 -0600

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,7 +5,7 @@ class Movie < ActiveRecord::Base
 	end
 
 	def Movie.getall_ratings arg1
-		@selected_movies = Movie.select(:rating).distinct
+		@selected_movies = Movie.all.rating.uniq
 		@selected_movies.each do |movie|
 			arg1 = movie.rating
 		end
